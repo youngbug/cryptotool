3e3665f5d12f439c912016457be9687b1a0ca38b
Zhao <cnrgc@163.com>
2024-08-21 08:34:22 +0800

    Add file.

---
+++ cryptotool/CCryptoAlgRsa.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CCryptoAlgRsa 对话框
+
+class CCryptoAlgRsa : public CDialogEx
+{
+	DECLARE_DYNAMIC(CCryptoAlgRsa)
+
+public:
+	CCryptoAlgRsa(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CCryptoAlgRsa();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_CRYPTOALG_RSA };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	CString m_Plain;
+	CString m_Cipher;
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+};
